a5de71a48bac6cc06c9417984e01b5433a2db872
William Haw <me@williamhaw.com>
2025-09-14 23:58:09 -0700

    Local openapi generator to v7.12.0

--- generate_from_docs.sh
+++ generate_from_docs.sh
@@ -1,2 +1,7 @@
 API_VERSION=$(cat docs.yaml | grep 'version: *' | cut -f2- -d:)
-openapi-generator generate -i docs.yaml -g python --additional-properties=packageName=trivium_python_sdk --additional-properties=packageVersion=$(echo $API_VERSION)
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.12.0 generate \
+-i /local/docs.yaml \
+-g python \
+--additional-properties=packageName=trivium_python_sdk \
+--additional-properties=packageVersion=$(echo $API_VERSION) \
+-o /local
