4b05360e7c663a30dcd8adaf84e50076d3f814e2
William Haw <me@williamhaw.com>
2025-10-05 14:03:21 -0700

    Update only version in README.md and setup.py

--- generate_from_docs.sh
+++ generate_from_docs.sh
@@ -1,4 +1,12 @@
 API_VERSION=$(cat docs.yaml | grep 'version: *' | cut -f2- -d:)
+
+# Update version in README.md
+sed -i '' "s/- API version: .*/- API version: $API_VERSION/" README.md
+sed -i '' "s/- Package version: .*/- Package version: $API_VERSION/" README.md
+
+# Update version in setup.py
+sed -i '' "s/VERSION = \".*\"/VERSION = \"$API_VERSION\"/" setup.py
+
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.12.0 generate \
 -i /local/docs.yaml \
 -g python \
